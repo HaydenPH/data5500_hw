2b916357fc2355d9d17a83cf84b76ba9c0e9de19
Hayden Hatch <haydenhatch@Haydens-MacBook-Air-6.local>
2024-10-04 21:53:49 -0600

    hw6

---
+++ in_class/oct2.py
@@ -0,0 +1,14 @@
+#Big O Notation
+
+
+lst = [3, 1, 4, 1, 5, 9, 2, 6]
+
+for 1 in lst:
+    print(1)
+
+
+def print_number(1, #soemthing_else):
+    print(#something)
+    print_number(#something)
+    
+print_number(1, #something)
